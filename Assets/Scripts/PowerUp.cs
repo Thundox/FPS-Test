8e7390bd2d2dd8a9d9271f518c25ba0d19572f61
Thundox <a95tofz5@gmail.com>
2023-03-02 13:28:54 +1100

    Power up Respawn + contents

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public int BoostRechargeAmount = 1;
+    public bool DoesRespawn;
+    public int RespawnTime;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerMovement>().BoostCharges += 1;
+            if (DoesRespawn == false)
+            {
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                return;
+            }
+            if (DoesRespawn == true)
+            {
+                StartCoroutine (PowerUpRespawn());
+                this.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    public IEnumerator PowerUpRespawn()
+    {
+
+        Debug.Log(this.name + " Has started respawn time " + RespawnTime);
+        yield return new WaitForSeconds(RespawnTime);
+        Debug.Log(this.name + " Has Respawned");
+        this.transform.GetChild(0).gameObject.SetActive(true);
+
+    }
+
+}
