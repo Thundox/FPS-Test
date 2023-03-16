37bbc17b8cd155dd80e698de29101067fc032ec5
Thundox <a95tofz5@gmail.com>
2023-03-16 14:11:12 +1100

    Health + Trap start + Boost Bug
    - Player now has health
    - Player sent to Respawn Point when health reaches 0
    - Basic Trap made reduces Player health by set amount once upon collision
    - Boosts no longer only go up by one, and now go up by boost recharge amount

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -11,7 +11,7 @@ public class PowerUp : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            other.GetComponent<PlayerMovement>().BoostCharges += 1;
+            other.GetComponent<PlayerMovement>().BoostCharges += BoostRechargeAmount;
             if (DoesRespawn == false)
             {
                 this.transform.GetChild(0).gameObject.SetActive(false);
