8dc31cd4a9668da8380c559ef5bffba0c0c3556f
Lincoln <krunkzzz@hotmail.com>
2023-04-13 12:05:27 +1000

    Turret
    Turret changes not yet 100% done.

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -7,14 +7,18 @@ public class Turret : MonoBehaviour
 {
     public int MyState = 0; // 0 = Idle, 1 = Target Player, 2 = Shoot
     public int DelayBeforeShooting = 2;
-    public float Counter = 0;
+    public float timeLeft = 3;
+    public float turnRate = 5;
     public LineRenderer MyLineRenderer;
     public GameObject DamageObject;
+    public Vector3 PlayerPosition;
+    public bool countDown;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            PlayerPosition = other.transform.position;
             MyState = 1; // Set State to Target
         }
     }
@@ -23,18 +27,14 @@ public class Turret : MonoBehaviour
     {
         if(other.tag == "Player")
         {
+            PlayerPosition = new Vector3(0,0,0);
             MyState = 0; // Set state to Idle
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        Counter = Counter += Time.deltaTime;
-        if(Counter >= DelayBeforeShooting) 
-        {
-            Counter = 0;
-            MyState = 2; // Shoot
-        }
+
     }
 
     private void Update()
@@ -45,14 +45,27 @@ public class Turret : MonoBehaviour
         }
         if(MyState == 1) // Target
         {
-            MyLineRenderer.enabled= true;
+            if (countDown)
+            {
+                timeLeft -= Time.deltaTime;
+                if(timeLeft <= 0)
+                {
+                    MyState = 2;
+                    timeLeft = 3;
+                }
+            }
+                MyLineRenderer.enabled= true;
+            Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(PlayerPosition), Time.deltaTime * turnRate);
         }
         if(MyState == 2) // Shoot
         {
-            GameObject newObject = Instantiate(DamageObject, transform.position, Quaternion.identity);
+
+            GameObject newObject = Instantiate(DamageObject, transform.position, transform.rotation);
             // put shoot function here.
             MyState = 1; // Set to Target
         }
     }
+
+    
 
 }
