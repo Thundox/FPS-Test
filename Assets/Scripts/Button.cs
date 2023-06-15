3eb4cb1b14c397b57307f1870723a2e919228032
Thundox <a95tofz5@gmail.com>
2023-06-15 13:09:39 +1000

    + Simple Button animation
    + Button animator done

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public Elevator MyElevator;
+    public Animator MyAnimator;
     private void OnTriggerEnter(Collider other)
     {
         MyElevator.TurnElevatorOn();
+        MyAnimator.SetTrigger("ButtonPressed");
     }
 }
